e2790f6ade14e932c3429ece7c36f20877bef80c
Nguyễn Huy Hoà <81904865+hoanguyen6611@users.noreply.github.com>
2022-05-10 09:21:22 +0700

    ui/ux netflix and api load data

---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => {
+      window.removeEventListener("scroll", transitionNavBar);
+    };
+  }, []);
+  return (
+    <div className={`nav ${!show && "nav__black"}`}>
+      <div className="nav__content">
+        <img
+          className="nav__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB8XDD0wkFQdsGp_xLQArOfA18wDZeaZDE",
+  authDomain: "netflix-app-e1519.firebaseapp.com",
+  projectId: "netflix-app-e1519",
+  storageBucket: "netflix-app-e1519.appspot.com",
+  messagingSenderId: "473812561425",
+  appId: "1:473812561425:web:11145b176fbf8b97a48bb0",
+  measurementId: "G-QJK4BQL3CD",
+};
